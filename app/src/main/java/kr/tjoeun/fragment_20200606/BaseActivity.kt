0b829c443079ec7bf6fdc02305794022f2782b06
janggalman <janggalman@gmail.com>
2020-06-06 11:19:13 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjoeun/fragment_20200606/BaseActivity.kt
@@ -0,0 +1,12 @@
+package kr.tjoeun.fragment_20200606
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
