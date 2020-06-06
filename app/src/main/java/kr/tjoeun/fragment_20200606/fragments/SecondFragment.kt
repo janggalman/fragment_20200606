dbfebb2a22824de142a60a1c91fde06bf8a5e190
janggalman <janggalman@gmail.com>
2020-06-06 12:15:16 +0900

    세컨드 프래그먼트 추가 및 스크롤뷰 사용법

---
+++ app/src/main/java/kr/tjoeun/fragment_20200606/fragments/SecondFragment.kt
@@ -0,0 +1,26 @@
+package kr.tjoeun.fragment_20200606.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_second.*
+import kr.tjoeun.fragment_20200606.R
+
+class SecondFragment :Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_second, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    
+        messageTxt.text ="반갑습니다"
+    }
+}
