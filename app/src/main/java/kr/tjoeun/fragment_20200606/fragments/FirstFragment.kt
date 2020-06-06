5da9ab5142f5a15c9594a27e03a30915fef1890b
janggalman <janggalman@gmail.com>
2020-06-06 11:38:13 +0900

    만들어준 프래그먼트를 액티비티에서 가져다 사용  - XML에 직접 배치하는 방법

--- app/src/main/java/kr/tjoeun/fragment_20200606/fragments/FirstFragment.kt
+++ app/src/main/java/kr/tjoeun/fragment_20200606/fragments/FirstFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_first.*
 import kr.tjoeun.fragment_20200606.R
 
 class FirstFragment : Fragment() {
@@ -15,6 +16,12 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        
+        messageTxt.text = "안녕하세요"
     }
 
 }
