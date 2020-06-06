522ea8fd20f58fc8680445e0af21b7f0beec2172
janggalman <janggalman@gmail.com>
2020-06-06 12:33:22 +0900

    뷰페이저 액티비티를 추가 => 앱의 시작점으로 활용

---
+++ app/src/main/java/kr/tjoeun/fragment_20200606/ViewPageActivity.kt
@@ -0,0 +1,23 @@
+package kr.tjoeun.fragment_20200606
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ViewPageActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_view_page)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
